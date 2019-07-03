292493d2c35bca9157d1cc8e6561bf8622c399e0
jackganesh <43372276+jackganesh@users.noreply.github.com>
2019-07-03 10:01:06 +0530

    Create progm3.py

---
+++ progm3.py
@@ -0,0 +1,7 @@
+ch=input("enter the char :")
+if ch in('A','E','I','O','U','a','e','i','o','u'):
+  print("vowel")
+elif ch in('B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','X','Y','Z''B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','X','Y','Z'):
+  print("consonant")
+else:
+  print("invalid")
